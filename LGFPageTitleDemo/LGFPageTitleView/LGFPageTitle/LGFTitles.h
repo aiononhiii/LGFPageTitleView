3022924f96858a37a7a356b48e561415bb45b21f
guofeng <452354033@qq.com>
2018-04-13 17:33:14 +0800

    添加帮助文字

--- LGFPageTitleDemo/LGFPageTitleView/LGFPageTitle/LGFTitles.h
+++ LGFPageTitleDemo/LGFPageTitleView/LGFPageTitle/LGFTitles.h
@@ -16,7 +16,13 @@
 #import "LGFMethod.h"
 #import "UIView+LGF.h"
 
-#define Bundle [NSBundle bundleWithPath:[[NSBundle bundleForClass:[self class]] pathForResource:@"LGFPageTitleView" ofType:@"bundle"]] ?: [NSBundle mainBundle]
+//********************** 帮助 ***************************
+//本控件是一个特殊的分页控件，实现了title和page的分离，方便一些喜欢自由搭配的产品，主要为一些复杂的页面布局而打造
+//本控件具体用法请参考该项目 https://github.com/aiononhiii/LGFPageTitleView
+//有问题可以在github上提给我哦～
+//******************************************************
+
+#define Bundle [NSBundle bundleWithPath:[[NSBundle bundleForClass:[self class]] pathForResource:@"LGFPageTitle" ofType:@"bundle"]] ?: [NSBundle mainBundle]
 #define RGB(A,B,C,D) [UIColor colorWithRed:A/255.0f green:B/255.0f blue:C/255.0f alpha:D]
 #define RandomColor RGB(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256), 1.0)
 #define MAIN(block) dispatch_async(dispatch_get_main_queue(),^{block});
